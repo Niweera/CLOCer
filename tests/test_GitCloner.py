d6e89141d34e7526b55ce925412f89863936b5b9
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-01-21 16:30:26 +0530

    Add Coverage for GitCloner.py

--- tests/test_GitCloner.py
+++ tests/test_GitCloner.py
@@ -1,5 +1,6 @@
+import shutil
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from git import Repo
 from clocer.CustomExceptions import CloneError
 from clocer.GitCloner import GitCloner
@@ -7,11 +8,14 @@ from os.path import exists, abspath, join, dirname, realpath
 
 
 class TestGitCloner(TestCase):
+    def setUp(self) -> None:
+        self.url = "https://github.com/Niweera/cpu-meter"
+
     def tearDown(self) -> None:
         GitCloner.clean_up()
 
     def test_clone_repo(self):
-        actual_url = "https://github.com/Niweera/cpu-meter"
+        actual_url = self.url
         GitCloner.clone_repo(actual_url)
 
         clone_path = abspath(
@@ -23,9 +27,25 @@ class TestGitCloner(TestCase):
         test_url = cloned_repo.remotes[0].config_reader.get("url")
         self.assertEqual(test_url, actual_url)
 
+    def test_clone_repo_fail(self):
+        temp_path: str = abspath(
+            join(dirname(dirname(realpath(__file__))), "clocer", "temp")
+        )
+        clone_path: str = abspath(join(temp_path, "clone"))
+
+        with patch("clocer.GitCloner.exists", Mock(return_value=True)):
+            if not exists(clone_path):
+                with self.assertRaises(CloneError):
+                    GitCloner.clone_repo(self.url)
+
+        with patch("clocer.GitCloner.exists", Mock(return_value=False)):
+            if exists(temp_path):
+                with self.assertRaises(CloneError):
+                    GitCloner.clone_repo(self.url)
+
     def test_clean_up_fail(self):
-        actual_error_msg = "Mock error occurred"
-        with patch.object(GitCloner, "clean_up", return_value=None) as get_mock:
-            with self.assertRaises(CloneError):
-                get_mock.side_effect = CloneError(actual_error_msg)
-                GitCloner.clean_up()
+        with patch("clocer.GitCloner.exists", Mock(return_value=True)), patch.object(
+            shutil, "rmtree"
+        ) as get_mock, self.assertRaises(CloneError):
+            get_mock.side_effect = Exception("Mock error occurred in shutil.rmtree()")
+            GitCloner.clean_up()
