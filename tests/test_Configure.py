e52e1a4567bf53fa0313142fb28aba030034272c
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-01-21 15:49:31 +0530

    Add Coverage for Configure.py

--- tests/test_Configure.py
+++ tests/test_Configure.py
@@ -1,5 +1,6 @@
+import subprocess
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from clocer.Configure import Configure
 from os.path import exists, abspath, join, dirname, realpath
 from clocer.CustomExceptions import ConfigureError
@@ -19,9 +20,25 @@ class TestConfigure(TestCase):
     def test_install_cloc(self):
         Configure.install_cloc()
 
-    def test_setup_output_fail(self):
-        actual_error_msg = "Mock error occurred"
-        with patch.object(Configure, "setup_output", return_value=None) as get_mock:
+    def test_check_cloc_fail(self):
+        with patch.object(subprocess, "check_output") as get_mock:
+            get_mock.side_effect = subprocess.CalledProcessError(1, "Mock command")
+            Configure.check_cloc()
+
+    def test_install_cloc_fail(self):
+        mock = Mock()
+        mock.returncode = Mock(return_value=1)
+
+        with patch.object(subprocess, "run") as get_mock:
+            get_mock.return_value = mock
             with self.assertRaises(ConfigureError):
-                get_mock.side_effect = ConfigureError(actual_error_msg)
-                Configure.setup_output()
+                Configure.install_cloc()
+
+    def test_setup_output_fail(self):
+        with patch("clocer.Configure.exists", Mock(return_value=False)):
+            output_path: str = abspath(
+                join(dirname(dirname(realpath(__file__))), "clocer", "output")
+            )
+            if exists(output_path):
+                with self.assertRaises(ConfigureError):
+                    Configure.setup_output()
