0a46a7b820b278cac24022b703ef0f00069fc384
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-01-21 15:09:05 +0530

    Add Coverage for CLOCer.py

--- tests/test_CLOCer.py
+++ tests/test_CLOCer.py
@@ -1,24 +1,30 @@
+import subprocess
 from unittest import TestCase
 from clocer.CLOCer import CLOCer
 import json
 from os.path import exists, abspath, join, dirname, realpath
-from unittest.mock import patch
-from clocer.CustomExceptions import CLOCerError
+from unittest.mock import patch, Mock
+
+from clocer.Configure import Configure
+from clocer.CustomExceptions import CLOCerError, ConfigureError
 
 
 class TestCLOCer(TestCase):
+    def setUp(self) -> None:
+        self.url = "https://github.com/Niweera/cpu-meter"
+
     def test_setup(self):
         CLOCer.setup()
 
     def test_setup_fail(self):
-        actual_error_msg = "Mock error occurred"
-        with patch.object(CLOCer, "setup", return_value=None) as get_mock:
+        actual_error_msg = "Mock ConfigureError occurred in Configure.setup_output()"
+        with patch.object(Configure, "setup_output", return_value=None) as get_mock:
+            get_mock.side_effect = ConfigureError(actual_error_msg)
             with self.assertRaises(CLOCerError):
-                get_mock.side_effect = CLOCerError(actual_error_msg)
                 CLOCer.setup()
 
     def test_run(self):
-        url = "https://github.com/Niweera/cpu-meter"
+        url = self.url
         repo_name = url.replace("https://github.com/", "").replace("/", "_")
 
         CLOCer.setup()
@@ -51,3 +57,13 @@ class TestCLOCer(TestCase):
 
         self.assertTrue(exists(output_path))
         self.assertDictEqual(test_result, actual_result)
+
+    def test_run_fail(self):
+        mock = Mock()
+        mock.returncode = Mock(return_value=1)
+        CLOCer.setup()
+
+        with patch.object(subprocess, "run") as get_mock:
+            get_mock.return_value = mock
+            with self.assertRaises(CLOCerError):
+                CLOCer.run(self.url)
